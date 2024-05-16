eb44beb8a9e0da5a26514e95037fbb17496f2a33
galiacheng <haixia.cheng@microsoft.com>
2024-05-16 16:53:13 +0800

    update README to use Postgre Flexible server.

--- src/test/aks/setup-env-variables-template.sh
+++ src/test/aks/setup-env-variables-template.sh
@@ -1,7 +1,9 @@
 export WLS_AKS_REPO_REF="2024-03-21-1-Q1" # oracle/weblogic-azure reference
 export RESOURCE_GROUP_NAME="abc1110rg" # customize this
 export STORAGE_ACCOUNT_NAME="stgwlsaks$(date +%s)" # storage account name
-export DB_SERVER_NAME="wlsdb$(date +%s)" # PostgreSQL server name
+export DB_SERVER_NAME="wlsserverdb$(date +%s)" # PostgreSQL server name
+export DB_NAME="wlsserverdb" # PostgreSQL database name
+export DB_USER="weblogic" # PostgreSQL database user
 export DB_PASSWORD="Secret123456" # PostgreSQL database password
 export MY_ORACLE_SSO_USER="user@contoso.com" # replace with your Oracle Account user name.
 export MY_ORACLE_SSO_PASSWORD="Secret123456" # replace with your Oracle Account password.
